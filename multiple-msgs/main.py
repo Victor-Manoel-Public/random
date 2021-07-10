0aa94b549a78b258c6d7368bcfc87ed371356490
Victor <81167569+Victor-Manoel-Public@users.noreply.github.com>
2021-07-10 16:26:21 -0300

    Update main.py

--- multiple-msgs/main.py
+++ multiple-msgs/main.py
@@ -1,12 +1,14 @@
 from time import sleep as sl
 from pyautogui import typewrite, press
 
+# this code can send multiples messages to multiples text messengers (WhatsApp, Instagram, Facebook, Telegram, etc).
 
-def sendMsg(msg="Hi", times=10):
-    while times >= 0:
-        typewrite(msg, 0.1)
-        press("enter")
-        times -= 1
+def sendMsg(msg="Hi", times=10): # creates the function with 2 parameters
+    while times >= 0: # while this condition is true, send the messages and hit the "enter" button.
+        typewrite(msg, 0.1) # same as pyautogui.typewrite().
+        press("enter") # same as pyautogui.press().
+        times -= 1 # here we decrement the times the messages timer.
 
-sl(3)
-sendMsg()
+sl(3) # wait 3 seconds before the program begin.
+sendMsg() # here we call our funcion without any parameters (). By defalut, the program will send "Hi" 10 times.
+
